935cfbef37f75641eb7051e85aba27fb1292e323
Brad Fabian <bradleofabian@gmail.com>
2020-10-28 16:49:56 -0400

    set up html, css and javscript

---
+++ js/app.js
@@ -0,0 +1,5 @@
+const setupDiv = document.getElementById('setup');
+const punchlinDiv  = document.getElementById('punchline');
+const punchlineBtn = document.getElementById('punchlineBtn');
+const newJokeBtn = document.getElementById('newJokeBtn');
+
